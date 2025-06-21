4332ae23a0002d5cf9a0cfe1291c2058145d222a
IverCoder <ivercoder@proton.me>
2025-06-21 19:33:59 +0800

    Set up scripted building
    Signed-off-by: IverCoder <ivercoder@proton.me>

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,7 +10,19 @@ export DTC_OVERLAY_TEST_EXT=$(pwd)/tools/mkdtimg/ufdt_apply_overlay
 export DTC_OVERLAY_VTS_EXT=$(pwd)/tools/mkdtimg/ufdt_verify_overlay_host
 export BSP_BUILD_ANDROID_OS=y
 
-make -C $(pwd) O=$(pwd)/out BSP_BUILD_DT_OVERLAY=y CC=clang LD=ld.lld ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- a3core_eur_open_defconfig -j12
-make -C $(pwd) O=$(pwd)/out BSP_BUILD_DT_OVERLAY=y CC=clang LD=ld.lld ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- -j12
+make -C $(pwd) O=$(pwd)/out BSP_BUILD_DT_OVERLAY=y CC=clang LD=ld.lld ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- a3core_eur_open_defconfig -j$(nproc --all)
+make -C $(pwd) O=$(pwd)/out BSP_BUILD_DT_OVERLAY=y CC=clang LD=ld.lld ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- -j$(nproc --all)
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+
+BUILD_OUTPUT="$(pwd)/out/arch/arm64/boot/Image"
+PUBLISH_DIR="$(pwd)/Publish"
+cd $PUBLISH_DIR
+rm boot.img -f
+tar -xvf a3core-A032FXXS6CXE1.tar.xz
+chmod +x magiskboot
+./magiskboot unpack stock-boot.img
+rm kernel -f
+cp $BUILD_OUTPUT kernel
+./magiskboot repack stock-boot.img boot.img
+rm $PUBLISH_DIR/kernel && rm $PUBLISH_DIR/ramdisk.cpio && rm $PUBLISH_DIR/dtb && rm $PUBLISH_DIR/stock-boot.img
